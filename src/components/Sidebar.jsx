1938935848868b6e384ae8034c8d196724b2f53a
jember08 <afandayul@gmail.com>
2021-09-21 13:48:25 +0700

    Mengubah warna tulisan clock in dan merubah warna icon user, dan reposisi icon lonceng

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import MenuItem from './MenuItem';
 import Category from '../assets/svg/Category.svg';
-import User from '../assets/svg/User.svg';
+import Users from '../assets/svg/Users.svg';
 import Frame from '../assets/svg/Frame.svg';
 import Notification from '../assets/svg/Notification.svg';
 
 const menuItems = [
   { name: 'Dashboard', to: '/DashboardPage', icon: Category },
-  { name: 'Staff', to: '/StaffPage', icon: User },
+  { name: 'Staff', to: '/StaffPage', icon: Users },
 ];
 
 const Sidebar = () => (
@@ -15,9 +15,12 @@ const Sidebar = () => (
     <div className='date-button'>
       <div className='date'>Jan 19, 2021 09:00 AM</div>
       <div className='button-Clock-in'>Clock In</div>
-      <img src={Notification} alt='Notification' />
     </div>
+
     <div className='header-role'>
+      <div className='notif'>
+        <img src={Notification} alt='Notification' />
+      </div>
       <img src={Frame} alt='Frame' />
       <div>
         <div className='header-staff'>Aldi Nugraha</div>
