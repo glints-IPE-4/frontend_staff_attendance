beefc3505177cd5e25d6abe3d3f30d2a0bc67aad
jember08 <afandayul@gmail.com>
2021-09-22 01:52:48 +0700

    kerangka staffpage/new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import DashboardPage from './pages/DashboardPage';
 import StaffPage from './pages/StaffPage';
 import LoginPage from './pages/LoginPage';
+import New from './components/New';
 import './styles/index.scss';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/login' component={LoginPage} />
         <Route path='/' exact component={Layout(DashboardPage)} />
         <Route path='/StaffPage' exact component={Layout(StaffPage)} />
+        <Route path='/StaffPage/New' exact component={Layout(New)} />
       </Switch>
     </Router>
   );
---
+++ src/components/New.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const New = () => <div>form</div>;
+
+export default New;
